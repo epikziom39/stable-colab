ca3f26ae7653acef5e0161491d19d17ad0752b0f
epikziom39 <105452781+epikziom39@users.noreply.github.com>
2023-03-11 21:08:25 +0100

    Update stable.sh

--- stable.sh
+++ stable.sh
@@ -1,6 +1,7 @@
 apt update 
 apt install git -y
 sudo apt install python3 python3-pip -y
+cd /home
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here https://dashboard.ngrok.com/get-started/your-authtoken (Copy and Ctrl+V to paste then press Enter): " CRP
