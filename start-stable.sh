ab932917a6d8e7f01bc04d5e9acade80c17fa74c
epikziom39 <105452781+epikziom39@users.noreply.github.com>
2023-03-11 21:24:09 +0100

    Update start-stable.sh

--- start-stable.sh
+++ start-stable.sh
@@ -5,6 +5,7 @@ nohup ./ngrok tcp -region $region 7860 &>/dev/null &
 cd /home/stable-diffusion-webui
 echo Your IP Address In "https://dashboard.ngrok.com/tunnels/agents"
 adduser stable
+su stable
 bash webui.sh
 echo sleep
 sleep 43200
