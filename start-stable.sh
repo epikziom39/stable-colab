6863d1c1e78909309a21e4f1e41022898c15903a
epikziom39 <105452781+epikziom39@users.noreply.github.com>
2023-03-11 21:10:31 +0100

    Update start-stable.sh

--- start-stable.sh
+++ start-stable.sh
@@ -1,12 +1,9 @@
-cd ..
-cd ..
-cd ..
+cd /home
 echo region "us, eu, au, ap, sa, jp, in"
 read -p "region: " region
 nohup ./ngrok tcp -region $region 7860 &>/dev/null &
 cd stable-diffusion-webui
-echo Your IP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo Your IP Address In "https://dashboard.ngrok.com/tunnels/agents"
 bash webui.sh
 echo sleep
 sleep 43200
